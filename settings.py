73124921874f8d48808f47b485c535ba87bd0d52
Stasya <144309321+stasya555@users.noreply.github.com>
2025-06-18 09:16:47 +0900

    Add files via upload

--- settings.py
+++ settings.py
@@ -132,12 +132,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
-}
+import os
+from pathlib import Path
+import dj_database_url
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': str(BASE_DIR / 'db.sqlite3'),  # обязательно строка, не Path
+        }
+    }
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
