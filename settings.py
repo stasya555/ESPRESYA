0d260ec0aa24fe8bbe5bc44abe1a4d4d743be0a7
Stasya <144309321+stasya555@users.noreply.github.com>
2025-06-18 09:10:12 +0900

    Add files via upload

--- settings.py
+++ settings.py
@@ -130,11 +130,13 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': BASE_DIR / 'db.sqlite3',
-      }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
